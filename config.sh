ad362b57af7f83241ffd043e586bc8ba01f2f4b8
Ankita <sankita@thoughtworks.com>
2020-03-07 16:26:17 +0530

    Read user input from environment variables

---
+++ config.sh
@@ -0,0 +1,8 @@
+export DISCOURSE_HOSTNAME="infrastructure-sandbox.com"
+export DISCOURSE_DEVELOPER_EMAILS="sankita@thoughtworks.com"
+export DISCOURSE_SMTP_ADDRESS="ankita.example.com"
+export DISCOURSE_SMTP_USER_NAME="ankitatest_username"
+export DISCOURSE_SMTP_PASSWORD="123"
+export DISCOURSE_SMTP_PORT="587"
+export LETSENCRYPT_ACCOUNT_EMAIL="me@example.com"
+
